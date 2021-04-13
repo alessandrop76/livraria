40753399f139a38c6aed5ce265f1f449502ed1d1
alessandro <alessandrodigima@gmail.com>
2021-04-12 21:51:46 -0300

    rota Author - gravação retorno e busca

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,9 @@
 module.exports = {
     MongoURI: 'mongodb+srv://root:relogio@cluster0.u93m9.gcp.mongodb.net/nutriclinic?retryWrites=true&w=majority'
 }
+
+
+//production
+// module.exports = {
+//     MongoURI: 'mongodb+srv://user:TUS4mKF$YKr5B2Q@livraria.br8ft.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// }
--- server.js
+++ server.js
@@ -5,24 +5,32 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
+//Routes
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+
+
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10MB', extended: false}))
+
+
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 
 const mongoose = require('mongoose');
 const db = require('./config/keys').MongoURI;
-
 // mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 // const db = mongoose.connection
 // db.on('error', error => console.error(err))
 // db.once('open', () => console.log('MongoDB Connected'))
-
 mongoose
 .connect (db, {
 useUnifiedTopology: true,
@@ -31,11 +39,6 @@ useNewUrlParser: true,
 .then (() => console.log ('MongoDB connected\n......................................................................................OK\n'))
 .catch (err => console.log ( err));
 
-
-
-
-
-app.use('/', indexRouter)
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, console.log(`Servidor rodando na porta ${PORT}\n
