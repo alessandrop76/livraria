9fec4c0a274315762b6eac4a0c383a97d7f3cd85
alessandro <alessandrodigima@gmail.com>
2021-04-12 22:35:21 -0300

    merge

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
     MongoURI: 'mongodb+srv://root:relogio@cluster0.u93m9.gcp.mongodb.net/nutriclinic?retryWrites=true&w=majority'
 }
 
@@ -8,7 +7,3 @@ module.exports = {
 // module.exports = {
 //     MongoURI: 'mongodb+srv://user:TUS4mKF$YKr5B2Q@livraria.br8ft.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 // }
-=======
-    MongoURI: '<yourconnectionMongo>'
-}
->>>>>>> c63c65bf743e753cd1d9bead00967dc1d4f610b8
